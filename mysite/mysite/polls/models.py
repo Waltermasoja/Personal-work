032a4b6bc9419f60f66ddb5f390a2bed487d8729
Walter Masoja <walterm@energypartners.co.za>
2023-05-25 04:36:25 +0200

    adding two tables in my models

--- mysite/mysite/polls/models.py
+++ mysite/mysite/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=250)
+    pud_date = models.DateTimeField('Published date')
+
+class Choice(models.Model):
+    question =models.ForeignKey(Question,on_delete=models.CASCADE)  
+    choice_text = models.CharField(max_length=250) 
+    votes = models.IntegerField(default=0)

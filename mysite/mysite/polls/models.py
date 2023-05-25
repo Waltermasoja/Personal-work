1750f68fe3ecbfe0cffd560d4fa3cabbde288e79
Walter Masoja <walterm@energypartners.co.za>
2023-05-25 08:13:38 +0200

    Correcting errors on the database

--- mysite/mysite/polls/models.py
+++ mysite/mysite/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=250)
-    pud_date = models.DateTimeField('Published date')
+    pub_date = models.DateTimeField('Published date')
 
 class Choice(models.Model):
     question =models.ForeignKey(Question,on_delete=models.CASCADE)  

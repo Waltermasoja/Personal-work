ef07ae144587f54deacfc5c1050031a0a41085b7
Walter Masoja <walterm@energypartners.co.za>
2023-05-30 13:40:09 +0200

    Adding class based views.

--- mysite/mysite/polls/views.py
+++ mysite/mysite/polls/views.py
@@ -3,26 +3,21 @@ from django.http import HttpResponse,Http404, HttpResponseRedirect
 from .models import Question,Choice
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    
-    context = {
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-        "latest_question_list":latest_question_list,
-    }
-    return render(request,"index.html",context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "detail.html"
 
-def detail(request,question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request,"detail.html",{"question":question})
-
-def results(request,question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+   model = Question
+   template_name = "results.html"
 
 def vote(request,question_id):
    question = get_object_or_404(Question, pk=question_id)

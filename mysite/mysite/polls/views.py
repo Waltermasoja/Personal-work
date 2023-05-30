59a90b0ec6ab8536918a1e128a7c6b883cdf6949
Walter Masoja <walterm@energypartners.co.za>
2023-05-30 08:09:26 +0200

    Addinf a view for question detail and a view for the choice action.

--- mysite/mysite/polls/views.py
+++ mysite/mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
-from .models import Question
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404, HttpResponseRedirect
+from .models import Question,Choice
 from django.template import loader
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -24,5 +25,19 @@ def results(request,question_id):
     return HttpResponse(response % question_id)
 
 def vote(request,question_id):
-    return HttpResponse("You are voting for question %s" % question_id)
-    
+   question = get_object_or_404(Question, pk=question_id)
+   try:
+       selected_choice = question.choice_set.get(pk=request.POST["choice"])
+       
+   except(KeyError,Choice.DoesNotExist):
+       return render(
+           request,
+          {"question": question,
+           "error_message" : "You did not select a choice."
+          })
+   else:
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
+       
